2f48dec56d34777f7a3650455ebe04e0cebfe996
dayeondayeon <dayeon2438@gmail.com>
2021-03-20 14:56:45 +0900

    Add Animator at Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,6 @@ public class Player : MonoBehaviour
     private float maxSpeed = 3f;
     private float jumpSpeed = 15f;
     private bool isJump = false;
-    Vector2 pos = new Vector2(0f, 1f);
     Rigidbody2D rigid;
     SpriteRenderer render;
    
@@ -19,12 +18,16 @@ public class Player : MonoBehaviour
     
     void Update()
     {
- 
+        if (Input.GetButtonUp("Horizontal")) //손 떼면 속도 줄임
+        {
+            // 벡터값이 방향뿐아니라 크기도 가지고 있기 때문에 정규화 필요(방향구할때쓰는거)
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        }
     }
     
     void FixedUpdate()
     {
-        float horizontal = Input.GetAxis("Horizontal"); //좌우로만 움직일거기 때문에 horizontal키값으로 받음
+        float horizontal = Input.GetAxisRaw("Horizontal"); //좌우로만 움직일거기 때문에 horizontal키값으로 받음
         rigid.AddForce(Vector2.right * horizontal, ForceMode2D.Impulse);
         if(rigid.velocity.x > maxSpeed) // 오른쪽 움직이는경우
         {
@@ -36,6 +39,7 @@ public class Player : MonoBehaviour
             render.flipX = true;
             rigid.velocity = new Vector2(maxSpeed *(-1), rigid.velocity.y);
         }
+
         //rigid.velocity.y를 해두는 이유는 얘를 0으로 주면 후에 점프하다 멈춤..
         if(Input.GetButtonDown("Jump") && isJump == false)
         {
